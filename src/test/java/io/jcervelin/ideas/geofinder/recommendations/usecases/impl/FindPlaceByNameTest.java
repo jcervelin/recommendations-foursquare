ec754779d5d2a04553db90e585b040bae5d2eefa
julianocervelintantalum <juliano.cervelin@tantalumcorporation.com>
2019-01-21 22:15:09 +0000

    Organize packages

--- src/test/java/io/jcervelin/ideas/geofinder/recommendations/usecases/impl/FindPlaceByNameTest.java
+++ src/test/java/io/jcervelin/ideas/geofinder/recommendations/usecases/impl/FindPlaceByNameTest.java
@@ -1,4 +1,4 @@
-package io.jcervelin.ideas.geofinder.recommendations.usecases;
+package io.jcervelin.ideas.geofinder.recommendations.usecases.impl;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
