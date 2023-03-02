4ea02c0f46d2107a334c9668523df7f93717a480
Juliano de Sousa Cervelin <juliano.cervelin@gmail.com>
2023-03-02 20:31:16 +0000

    Corrigindo erro gramatical

--- src/main/java/io/jcervelin/ideas/geofinder/recommendations/gateways/https/RecommendationsController.java
+++ src/main/java/io/jcervelin/ideas/geofinder/recommendations/gateways/https/RecommendationsController.java
@@ -17,7 +17,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/recommendations")
 @RequiredArgsConstructor
-@Api(value = "/api/recommendations", description = "Recommendation system using Foursquare API")
+@Api(value = "/api/recommendations", description = "Recommendations system using Foursquare API")
 public class RecommendationsController {
 
     private final FindPlaceByName findPlaceByName;
